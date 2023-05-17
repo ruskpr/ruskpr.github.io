875fdc373bcc189ac7d0ef668c13b925229214d8
Russ Koprulu <rustulp@gmail.com>
2023-05-17 03:45:30 -0600

    updated styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,29 +1,78 @@
+import React from "react";
+
 // create ButtonProps interface
 interface ButtonProps {
-  children: React.ReactNode;
-  type: ButtonType;
+  children?: React.ReactNode;
+  type?: ButtonType;
+  size?: ButtonSize;
+  animate?: boolean;
   onClick?: () => void;
   className?: string;
+  href?: string;
 }
 
 // create an enum for button types
 export enum ButtonType {
-  PRIMARY = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
-  SECONDARY = "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded",
-  SUCCESS = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded",
-  DANGER = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
-  WARNING = "bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded",
-  INFO = "bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded",
-  LIGHT = "bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded",
-  DARK = "bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded",
+  Primary = "bg-red-400 hover:bg-red-500",
+  Secondary = "bg-indigo-500 hover:bg-indigo-700",
+  Success = "bg-green-500 hover:bg-green-700",
+  Danger = "bg-rose-800 hover:bg-rose-900",
+  Warning = "bg-yellow-500 hover:bg-yellow-600",
+  Info = "bg-cyan-400 hover:bg-cyan-300",
+  Light = "bg-gray-200 hover:bg-gray-300 text-black",
+  Dark = "bg-gray-700 hover:bg-gray-900",
+  Link = "font-extralight text-white bg-transparent hover:text-red-300 hover:shadow-none",
+}
+
+export enum ButtonSize {
+  xs = "py-1 px-3 text-xs",
+  sm = "py-1 px-3 text-md",
+  md = "py-1 px-3 text-lg",
+  lg = "py-1 px-4 text-xl",
+  xl = "py-2 px-6 text-3xl",
 }
 
 // create a button component
 export const Button = (props: ButtonProps) => {
-  const { children, onClick, className, type } = props;
+  const {
+    children,
+    onClick,
+    className,
+    type = ButtonType.Primary,
+    size = ButtonSize.md,
+    animate = true,
+    href,
+  } = props;
+
+  const defaultStyles =
+    "flex items-center text-sm font-primary font-bold text-white font-medium rounded-md";
+  const hoverTransition = animate
+    ? "transition duration-75 ease-in-out hover:shadow-lg hover:scale-105"
+    : "";
+
   return (
-    <button onClick={onClick} className={type + " " + className}>
-      {children}
+    <button
+      {...(href ? { href } : {})}
+      onClick={onClick}
+      className={
+        hoverTransition +
+        " " +
+        defaultStyles +
+        " " +
+        type +
+        " " +
+        size +
+        " " +
+        className
+      }
+    >
+      {href ? (
+        <a href={href} className="flex items-center">
+          {children}
+        </a>
+      ) : (
+        children
+      )}
     </button>
   );
 };
