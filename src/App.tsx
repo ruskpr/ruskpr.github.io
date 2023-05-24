d892ef56de98c8909a84954eaa1b40bb4dc78821
Russ Koprulu <rustulp@gmail.com>
2023-05-24 04:21:06 -0600

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import AppRouter from "./components/layout/AppRouter";
 
 function App() {
   return (
-    <div className="bg-gray-950 h-screen">
+    <div className="bg-gray-950">
       <Header />
       <div className="container mx-auto flex justify-center items-center">
         <AppRouter />
       </div>
+      <Footer />
     </div>
   );
 }
