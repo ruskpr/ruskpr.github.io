d53cb45f1bef9f4ab22e1e14e2f7533cdad42907
Russ Koprulu <rustulp@gmail.com>
2023-05-24 04:21:13 -0600

    add project detail page

--- src/components/layout/AppRouter.tsx
+++ src/components/layout/AppRouter.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import ContactPage from "../pages/contact-page/ContactPage";
 import HomePage from "../pages/home-page/HomePage";
 import ProjectsPage from "../pages/projects-page/ProjectsPage";
+import ProjectDetailPage from "../pages/projects-page/ProjectDetailPage";
 import AboutPage from "../pages/about-page/AboutPage";
 
 export default function AppRouter() {
@@ -11,6 +12,7 @@ export default function AppRouter() {
     <Routes>
       <Route path="/" Component={HomePage} />
       <Route path="/projects" Component={ProjectsPage} />
+      <Route path="/projects/:slug" Component={ProjectDetailPage} />
       <Route path="/about" Component={AboutPage} />
       <Route path="/contact" Component={ContactPage} />
     </Routes>
