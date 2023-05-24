6197bb5b4936dce4a9a19152f0606b18d10539d5
Russ Koprulu <rustulp@gmail.com>
2023-05-23 20:19:23 -0600

    Added project list

--- src/components/pages/projects-page/ProjectsPage.tsx
+++ src/components/pages/projects-page/ProjectsPage.tsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectCardContainer from "./ProjectCardContainer";
+import { IProjectData, getDataFromFile } from "./Utils";
+
 export default function ProjectsPage() {
+  const [projectData, setProjectData] = useState<IProjectData[]>([]);
+
+  let renderedProjectCards: JSX.Element[] = [];
+
+  useEffect(() => {
+    var data = getDataFromFile();
+    setProjectData(data);
+  }, []);
+
+  renderedProjectCards = projectData.map((proj) => {
+    return <ProjectCard project={proj} key={proj.id} />;
+  });
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">
-        Projects Page
-      </h1>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-white mt-16 mb-48">Projects</h1>
+
+      <ProjectCardContainer>{renderedProjectCards}</ProjectCardContainer>
     </div>
   );
 }
