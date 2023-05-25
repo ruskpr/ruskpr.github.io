8cff4248662fb3506e38fa04a6bc886dddb57586
Russ Koprulu <rustulp@gmail.com>
2023-05-25 04:53:17 -0600

    style changes + zooming feature

--- src/components/pages/projects-page/ProjectDetailCarousel.tsx
+++ src/components/pages/projects-page/ProjectDetailCarousel.tsx
@@ -33,7 +33,7 @@ export default function ProjectDetailCarousel(props: {
           >
             <img
               style={{ objectFit: "contain" }}
-              className="w-32 h-20 rounded-xl bg-gray-200"
+              className="w-32 lg:w-40 h-24 lg:h-32 rounded-xl bg-gray-200"
               src={img}
               alt={(index + 1).toString()}
             />
@@ -62,15 +62,50 @@ export default function ProjectDetailCarousel(props: {
     }
   };
 
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const carousel = carouselRef.current;
+    // // zoom in when mouse is over carousel
+    if (carousel) {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      const carouselX = carousel.getBoundingClientRect().x;
+      const carouselY = carousel.getBoundingClientRect().y;
+      const carouselWidth = carousel.getBoundingClientRect().width;
+      const carouselHeight = carousel.getBoundingClientRect().height;
+
+      const x = (mouseX - carouselX) / carouselWidth;
+      const y = (mouseY - carouselY) / carouselHeight;
+
+      const scale = 1.6;
+      const translateX = (x - 0.5) * (scale - 1) * 100 * -1;
+      const translateY = (y - 0.5) * (scale - 1) * 100 * -1;
+
+      carousel.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%)`;
+    }
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    const carousel = carouselRef.current;
+    if (carousel) {
+      carousel.style.transform = `scale(1) translate(0%, 0%)`;
+    }
+  };
+
   return (
     <div>
       <div
-        ref={carouselRef}
-        className="carousel rounded-3xl w-full lg:w-8/12 bg-gray-300"
+        className="overflow-hidden rounded-3xl w-full bg-gray-300"
+        onMouseLeave={(e) => handleMouseLeave(e)}
       >
-        {renderedCarouselImages.length > 0 ? renderedCarouselImages : <></>}
+        <div
+          ref={carouselRef}
+          onMouseMove={(e) => handleMouseMove(e)}
+          className="carousel"
+        >
+          {renderedCarouselImages.length > 0 ? renderedCarouselImages : <></>}
+        </div>
       </div>
-      <div className="flex rounded-3xl mt-3 justify-center w-full lg:w-8/12 py-2 gap-2">
+      <div className="flex rounded-3xl mt-3 justify-center lg:justify-start w-full py-2 gap-2">
         {renderedCarouselButtons.length > 0 ? renderedCarouselButtons : <></>}
       </div>
     </div>
