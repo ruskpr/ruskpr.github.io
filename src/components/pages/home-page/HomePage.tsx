856267a6e05d29f200f3c141c33de974ce7db75b
Russ Koprulu <rustulp@gmail.com>
2023-05-25 04:53:54 -0600

    style changes

--- src/components/pages/home-page/HomePage.tsx
+++ src/components/pages/home-page/HomePage.tsx
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center items-start w-full">
+    <div className="flex justify-center items-start w-full mb-48">
       <Hero />
     </div>
   );
