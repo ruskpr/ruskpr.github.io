182b056ddff62084bd5d8a5eff0acc65b6532740
Russ Koprulu <rustulp@gmail.com>
2023-07-06 19:24:18 -0600

    styling changes

--- src/components/pages/home-page/SkillsSection.tsx
+++ src/components/pages/home-page/SkillsSection.tsx
@@ -30,20 +30,24 @@ export default function SkillsSection() {
 
   return (
     <section className="h-fit w-full mt-8 md:mt-20 lg:mt-48">
-      <h2 className="text-4xl text-center font-primary font-normal text-white">
+      <h2 className="text-3xl sm:text-4xl text-center font-primary font-normal text-white">
         My Skills
       </h2>
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center mb-4 sm:mb-0">
         <div className="w-48 border-b border-blue-600" />
       </div>
       <br />
 
       {/* FRONTEND */}
       <article className="text-white mb-24">
-        <h1 className="text-6xl font-primary pl-4 ">Frontend</h1>
-        <div className="w-72 border-b ml-4 border-blue-600 mb-8" />
+        <h1 className="text-4xl sm:text-6xl font-primary pl-0 sm:pl-4 text-center sm:text-left ">
+          Frontend
+        </h1>
+        <div className="flex justify-center sm:justify-start">
+          <div className="w-72 border-b ml-0 sm:ml-4 border-blue-600 mb-8" />
+        </div>
 
-        <div className="text-center grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-12 place-items-center lg:gap-8 w-full">
+        <div className="text-center grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-2 place-items-center lg:gap-8 w-full">
           <div>
             <div className={iconDivStyle}>
               <SiHtml5 className="text-orange-600 z-10" />
@@ -117,7 +121,7 @@ export default function SkillsSection() {
           <div className="w-64 border-b mr-4 border-blue-600 mb-8" />
         </div>
 
-        <div className="text-center grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-12 place-items-center lg:gap-8 w-full">
+        <div className="text-center grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-2 place-items-center lg:gap-8 w-full">
           <div>
             <div className={iconDivStyle}>
               <SiNextdotjs className="text-zinc-800 z-10" />
@@ -181,8 +185,6 @@ export default function SkillsSection() {
             </div>
             <span className="font-primary font-semibold">Blazor</span>
           </div>
-
-          
         </div>
       </article>
     </section>
