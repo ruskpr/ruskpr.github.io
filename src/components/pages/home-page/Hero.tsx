c7dd7bb860711fa85c26a3475c6a8d6be2409e4e
Russ Koprulu <rustulp@gmail.com>
2023-07-12 02:12:13 -0600

    fix homepage styling

--- src/components/pages/home-page/Hero.tsx
+++ src/components/pages/home-page/Hero.tsx
@@ -3,7 +3,7 @@ import HeroProjectList from "./HeroProjectList";
 
 export default function Hero() {
   return (
-    <div className="h-fit lg:h-96 hero mt-8 md:mt-20 lg:mt-48 hero-content flex flex-col lg:flex-row justify-between items-start lg:items-center">
+    <div className="w-full h-fit lg:h-96 hero mt-8 md:mt-20 lg:mt-48 p-4 flex flex-col lg:flex-row justify-between items-start lg:items-center">
       <div className="w-full h-full flex flex-col items-start">
         <h1 className="text-5xl lg:text-7xl font-primary font-normal text-white mb-4">
           Rustu Koprulu
